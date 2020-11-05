32030ca45996ec112cadb7e0afb21fd2db8c3cbe
Soumikpal9 <soumikpal1997@gmail.com>
2020-11-05 22:21:59 +0530

    [SP]Added: Contact Details To Be Added

---
+++ src/main/java/AddressBookWorkshop/AddressBook.java
@@ -0,0 +1,22 @@
+package AddressBookWorkshop;
+import java.util.*;
+
+public class AddressBook {
+private List<ContactDetails> addBook = new ArrayList<>();
+	
+	public void setAddressBook(List<ContactDetails> addBook) {
+		this.addBook = addBook;
+	}
+	
+	public List<ContactDetails> getAddressBook(){
+		return addBook;
+	}
+	
+	public void addContact(ContactDetails contact) {
+		addBook.add(contact);
+	}
+	
+	public List<ContactDetails> showContact(){
+		return addBook;
+	}
+}
---
+++ src/main/java/AddressBookWorkshop/ContactDetails.java
@@ -0,0 +1,28 @@
+package AddressBookWorkshop;
+
+public class ContactDetails {
+	public String firstName;
+	public String lastName;
+	public String address;
+	public String city;
+	public String state;
+	public String zipcode;
+	public String phone;
+	public String email;
+	
+	public ContactDetails(String firstName, String lastName, String address, String city, String state, String zipcode, String phone, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zipcode = zipcode;
+		this.phone = phone;
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "First Name: "+firstName+"\nLast Name: "+lastName+"\nAddress: "+address+"\nCity: "+city+"\nState: "+state+"\nZip: "+zipcode+"\nPhone Number: "+phone+"\nEmail: "+email;
+	}
+}
